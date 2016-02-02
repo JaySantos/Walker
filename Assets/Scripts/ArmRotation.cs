15d4fe63c52e69671f39461755b46d6eae951fef
Jay Santos <jay@unity3d.com>
2016-02-02 15:28:42 -0200

    First Commit
    - Gun follows mouse
    - Robot moves

---
+++ Assets/Scripts/ArmRotation.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmRotation : MonoBehaviour
+{
+    public float topXLimit = 300f;
+    public float bottomXLimit = 40f;
+    public float bottomYLimit = 90f;
+    public float topYLimit = 100f;
+
+    private float camRayLength = 20f;
+    private LayerMask hitMask;
+
+    private Vector3 finalAngle;
+
+    void Awake()
+    {
+        hitMask = LayerMask.GetMask("CamHit");
+        finalAngle = new Vector3(0f, 0f, 0f);
+    }
+
+    void FixedUpdate()
+    {
+    }
+
+    void LateUpdate()
+    {
+        TurnArm();
+    }
+
+    void TurnArm()
+    {
+        // Create a ray from the mouse cursor on screen in the direction of the camera.
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Create a RaycastHit variable to store information about what was hit by the ray.
+        RaycastHit camHit;
+
+        // Perform the raycast and if it hits something on the floor layer...
+        if (Physics.Raycast(camRay, out camHit, camRayLength, hitMask))
+        {
+            // Create a vector from the player to the point on the floor the raycast from the mouse hit.
+            Vector3 armToMouse = camHit.point - transform.position;
+
+            // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+            Quaternion newRotation = Quaternion.LookRotation(armToMouse);
+            Vector3 rawAngle = newRotation.eulerAngles;
+            if (rawAngle.x > 180f)
+            {
+                finalAngle.x = Mathf.Clamp(rawAngle.x, topXLimit, 360f);
+            }
+            else
+            {
+                finalAngle.x = Mathf.Clamp(rawAngle.x, 0, bottomXLimit);
+            }
+            
+            finalAngle.y = Mathf.Clamp(rawAngle.y, bottomYLimit, topYLimit);
+            finalAngle.z = rawAngle.z;
+            newRotation = Quaternion.Euler(finalAngle);
+            transform.rotation = newRotation;
+        }
+    }
+}
---
+++ Assets/Scripts/MechoController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.Animations;
+
+public class MechoController : MonoBehaviour
+{
+    public float speed = 1f;
+
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Moving forward
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+            anim.SetBool("MoveFwd", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetBool("MoveFwd", false);
+        }
+
+        //Moving backwards
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+            anim.SetBool("MoveBack", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetBool("MoveBack", false);
+        }
+    }
+}
